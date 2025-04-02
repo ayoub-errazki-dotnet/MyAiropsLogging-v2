b560ef1604cdce3841c5f43ec1a7bd6faad88453
ayoub-errazki-dotnet <ayouberrazkiuk2@gmail.com>
2025-04-02 13:38:35 +0200

    Initial Commit

---
+++ Logging.Microservice/Shared/LogDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Logging.Microservice.Shared
+{
+    public enum Level { Fatal, Error, Warning, Information };
+    public class LogDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [MaxLength(255)]
+        public required string Message { get; set; }
+
+        public required Level Level { get; set; }
+
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+    }
+}
